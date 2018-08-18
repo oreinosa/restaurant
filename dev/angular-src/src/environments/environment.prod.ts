2b87f04a84b99d388a74fc73afa018e6be27c146
oreinosa <31995668+oreinosa@users.noreply.github.com>
2018-08-18 16:25:51 -0600

    update

--- dev/angular-src/src/environments/environment.prod.ts
+++ dev/angular-src/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'http://localhost/api/v1',
-  static: 'http://localhost/static',
+  api: 'http://localhost/api/v1/',
+  static: 'http://localhost/static/',
 };
