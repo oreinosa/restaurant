66eb29e7a0d792d63b486bea71a644ff11db1e4c
oreinosa <31995668+oreinosa@users.noreply.github.com>
2018-08-18 16:33:47 -0600

    update

--- dev/angular-src/src/environments/environment.prod.ts
+++ dev/angular-src/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'http://thenewfuturesv.com/api/v1/',
-  static: 'http://thenewfuturesv.com/static/',
+  api: 'https://thenewfuturesv.com/api/v1/',
+  static: 'https://thenewfuturesv.com/static/',
 };
